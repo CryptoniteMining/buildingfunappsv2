a7a52edc9d17b0f55e2802a919f55a09477845cc
WesleyETH <87329637+CryptoniteMining@users.noreply.github.com>
2025-04-30 16:20:57 -0400

    Update ensUtils.js

--- utils/ensUtils.js
+++ utils/ensUtils.js
@@ -1,7 +1,7 @@
 import { ethers } from 'ethers';
 
 export async function getEnsData(name) {
-  const provider = new ethers.JsonRpcProvider(`https://eth-mainnet.g.alchemy.com/v2/innCpgwBD8GBgVLWJV1cLq41vhob1He1`);
+  const provider = new ethers.JsonRpcProvider('https://eth-mainnet.g.alchemy.com/v2/innCpgwBD8GBgVLWJV1cLq41vhob1He1');
   const address = await provider.resolveName(name);
 
   if (!address) {
