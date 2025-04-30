4162f86a3d097ca1ba22492bbf552964fc921258
WesleyETH <87329637+CryptoniteMining@users.noreply.github.com>
2025-04-30 15:21:55 -0400

    Delete utils directory

--- utils/ensUtils.js
+++
@@ -1,9 +0,0 @@
-
-export async function getEnsData(name) {
-  const response = await fetch(`https://metadata.ens.domains/mainnet/${name}`);
-  if (!response.ok) {
-    throw new Error('Failed to fetch metadata');
-  }
-  const data = await response.json();
-  return data;
-}
