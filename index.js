3ac4ab11365d8dbb2da6384bb0c430370ed5e90e
WesleyETH <87329637+CryptoniteMining@users.noreply.github.com>
2025-04-30 15:02:12 -0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-// lookup.xyz – Final Product
-export default function Home() {
-  return (
-    <div className="p-10 text-center text-2xl font-bold">
-      Final version of lookup.xyz loaded. ENS + EFP + donation footer live.
-    </div>
-  );
-}
