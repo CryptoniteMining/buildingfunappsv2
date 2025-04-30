d343fc496449670f3482c93398e6d3bdcb0ee9d8
WesleyETH <87329637+CryptoniteMining@users.noreply.github.com>
2025-04-30 15:33:28 -0400

    Update ens.js

--- pages/api/ens.js
+++ pages/api/ens.js
@@ -1,5 +1,5 @@
 
-import { getEnsData } from '../../../utils/ensUtils';
+import { getEnsData } from '../../utils/ensUtils';
 
 export default async function handler(req, res) {
   const { name } = req.query;
