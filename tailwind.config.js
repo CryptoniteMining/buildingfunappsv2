685f2827a3f0db117407c0e9d1122ad05ac080cb
WesleyETH <87329637+CryptoniteMining@users.noreply.github.com>
2025-04-30 15:57:26 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      animation: {
+        'raindrop': 'raindrop 0.6s ease-out forwards',
+      },
+      keyframes: {
+        raindrop: {
+          '0%': { transform: 'scale(0.2)', opacity: '0.9' },
+          '100%': { transform: 'scale(1.6)', opacity: '0' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
